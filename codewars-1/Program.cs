b1f275a3dc07c70756012d099eb69b457cd6d998
HornyFerret <ombro.de.morto@gmail.com>
2021-07-09 18:26:38 +0300

    Добавьте файлы проекта.

---
+++ codewars-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace codewars_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string str = RepeatStr(n, s);
+            Console.WriteLine(str);
+        }
+        public static string RepeatStr(int n, string s)
+        {
+            string str = "";
+            for (int i = 1; i <= n; i++)
+            {
+                str += s;
+            }
+            return str;
+        }
+
+    }
+}
